c45cae09c7ca85dbfbbe3afb7dd4ed87f06b9ffe
tusharjari <tusharjari8201@gmail.com>
2021-12-18 20:50:02 +0530

    secoffour

---
+++ Assignment2/4/model/student.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const Student = new mongoose.Schema({
+  name: { type: String, default: null },
+  email: { type: String, default: null },
+  password: { type: String, default: null },
+  address: { type: String, default: null },
+});
+module.exports = mongoose.model("Student", Student);
