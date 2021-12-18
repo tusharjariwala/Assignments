8874160c83994c6da65c561e9e3397f13f4c7c19
tusharjari <tusharjari8201@gmail.com>
2021-12-18 20:13:54 +0530

    third

---
+++ Assignment1/3/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app).listen(3000);
+const io = require("socket.io")(http);
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+var cricketdata = require("./cricket.json");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+io.on("connection", (socket) => {
+  console.log("User is connected");
+  
+  socket.on("getData", (data) => {
+    for (var i = 0; i < cricketdata.innings.length; i++) {
+      console.log(i);
+      if (cricketdata.innings[i].team == data.team) {
+    
+        console.log(cricketdata.innings[i].overs[data.overnumber].deliveries);
+        socket.emit(
+          "sendData",
+          cricketdata.innings[i].overs[data.overnumber].deliveries
+        );
+        break;
+      } else {
+        socket.emit("sendData", []);
+      }
+    }
+  });
+});
